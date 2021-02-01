0adbd163e64f75bb4fcda12b8d53d1a10e8738a8
Rhaxis <ville.ekholm88@gmail.com>
2021-02-01 18:11:25 +0200

    Update App.js

--- App.js
+++ App.js
@@ -1,34 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React, {useState} from 'react';
-import { StyleSheet, Text, View, ScrollView, FlatList } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React, { useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  FlatList,
+  Button,
+} from "react-native";
 import Constants from "expo-constants";
 import uuid from "uuid";
 
-// Importataan omat moduulit järjestelmämoduulien jälkeen
 import InputText from "./Components/InputText";
 import ItemList from "./Components/ItemList";
+import EditTask from "./Components/EditTask";
+import CountdownTimer from "./Components/CountdownTimer";
 
 export default function App() {
-  // Tallennetaan käyttäjän syöttämä teksti siten, että se ei häviä uudelleen piirron
-  // yhteydesä
-
   const [tasks, setTasks] = useState([]);
 
   const [userInput, setUserInput] = useState("");
 
-  const addTextHandler = (text) => {
-    setUserInput(userInput + " " + text);
-  };
+  const [isEditViewVisible, setEditViewVisibility] = useState(false);
+
+  const [selectedTask, setSelectedTask] = useState(undefined);
 
   // ... spread operator. Pulls items out of an array. In our case it is used to create a
   // new array
   const addTaskHandler = (task) => {
-    setTasks([...tasks, {key: uuid.v4(), text: task}]);
-  }
+    if (selectedTask !== undefined) {
+      selectedTask.text = task;
+    } else {
+      setTasks([...tasks, { key: uuid.v4(), text: task }]);
+    }
 
-  // Luodaan uusi taulukko johon laitetaan kaikki task taulukon sisältö paitsi se joka vastaa key arvoa
-   const removeTaskHandler = (key) => {
+    showEditView(false);
+  };
+
+  // removes an item from the array based on the item key using the Array.filter method
+  // function is given to ItemList props as onItemPress
+  const removeTaskHandler = (key) => {
     setTasks(tasks.filter((task) => task.key !== key));
+  };
+
+  const onItemPressed = (key) => {
+    let currentTask = tasks.find((task) => task.key == key);
+    setSelectedTask(currentTask);
+    showEditView(true);
+  };
+
+  const showEditView = (isShown) => {
+    if (!isShown) {
+      setSelectedTask(undefined);
+    }
+    setEditViewVisibility(isShown);
+  };
+
+  const onTimerEnd = () => {
+    console.log("Timer stopped");
   };
 
   return (
@@ -36,9 +65,19 @@ export default function App() {
       <View style={stylesLight.statusBar}>
         <StatusBar style="auto" />
       </View>
-
-      <InputText submitText="OK" onSubmitPressed={addTaskHandler} />
-      <ItemList data={tasks} onItemPress={removeTaskHandler} />
+      <ItemList
+        data={tasks}
+        onItemPress={onItemPressed}
+        onLongPress={removeTaskHandler}
+      />
+      <EditTask
+        onSubmitPressed={addTaskHandler}
+        isVisible={isEditViewVisible}
+        closeView={() => showEditView(false)}
+        text={selectedTask !== undefined ? selectedTask.text : undefined}
+      />
+      <CountdownTimer startingTime={10} onTimerEnd={onTimerEnd} />
+      <Button title="Add task" onPress={() => showEditView(true)} />
     </View>
   );
 }
@@ -49,15 +88,15 @@ const stylesLight = StyleSheet.create({
     justifyContent: "flex-start",
     backgroundColor: "white",
     height: "100%",
-    paddingTop: Constants.statusBarHeight
+    paddingTop: Constants.statusBarHeight,
   },
   statusBar: {
     // height: Constants.statusBarHeight
   },
   text: {
     width: 100,
-    color: "black"
-  }
+    color: "black",
+  },
 });
 
 const stylesDark = StyleSheet.create({
@@ -69,6 +108,6 @@ const stylesDark = StyleSheet.create({
   },
   text: {
     width: 100,
-    color: "#FFF"
-  }
+    color: "#FFF",
+  },
 });
